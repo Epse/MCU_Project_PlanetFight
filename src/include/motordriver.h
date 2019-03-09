ea7a7b33879ea75955a40226945b1e4b04978808
Stef Pletinck <s@stefpletinck.be>
2019-03-09 17:43:40 +0100

    Basic function for motor driver and PWM
    Also reserved my pin

---
+++ src/include/motordriver.h
@@ -0,0 +1,9 @@
+#ifndef MOTORDRIVER_H
+#define MOTORDRIVER_H
+
+void motorInit();
+void motorOn();
+void motorOff();
+void motorPower(uint8_t power);
+
+#endif
---
+++ src/motordriver.c
@@ -0,0 +1,28 @@
+#include <avr/io.h>
+#include "include/motordriver.h"
+
+void motorInit() {
+  // Set TIMER0 to fast PWM
+  TCCR0A = _BV(WGM00) | _BV(WGMO1);
+  TCCR0B = _BV(WGM02);
+  // Falling edge on counter match (So counter limit is the pulse width)
+  TTCR0A |= _BV(COM0A1);
+  // Set OC01 to output
+  DDRB |= _BV(DDB7);
+  // Set pulse width to 50%
+  OCR0A = 128;
+}
+
+void motorOn() {
+  // Turn Timer/Counter 0 on and let it run at fck without prescaler
+  TCCR0B |= _BV(CS00);
+}
+
+void motorOff() {
+  // Disable all CS bits
+  TCCR0B &= 0b11111000;
+}
+
+void motorPower(uint8_t power) {
+  OCR0A = power;
+}
