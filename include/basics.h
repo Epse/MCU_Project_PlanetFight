4f2edf703870e04cb8ae87b1e5d29f4f42c5a7b9
Stef Pletinck <s@stefpletinck.be>
2019-03-28 13:55:30 +0100

    added input shortcuts

--- include/basics.h
+++ include/basics.h
@@ -63,5 +63,7 @@
 #define output_low(port,pin) port &= ~(1<<pin)
 #define output_high(port,pin) port |= (1<<pin)
 #define set_output(portdir,pin) portdir |= (1<<pin)
+#define set_input(portdir,pin) portdir &= ~(1<<pin)
+#define set_pullup(portdir,pin) portdir |= (1<<pin)
 
 #endif	//DWENGUINO_BOARD_H_
