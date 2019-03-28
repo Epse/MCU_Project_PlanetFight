a8044b336355cd73929255cdc4092f8541029e32
Stef Pletinck <s@stefpletinck.be>
2019-03-28 13:55:36 +0100

    Add Joystick handling
    See #2
    Added a bunch of functions for handling Joystick
    status and information about which switches have risen.
    TODO: check if the tick delay inherent in engine is enough
    or if extra debounce is necessary.

---
+++ lib/Joystick/src/Joystick.h
@@ -0,0 +1,19 @@
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+#include <avr/io.h>
+#include <stdint.h>
+
+typedef uint8_t JoyStatus;
+
+void init();
+void tick();
+
+uint8_t joystick_is_up(JoyStatus s);
+uint8_t joystick_is_down(JoyStatus s);
+uint8_t joystick_is_left(JoyStatus s);
+uint8_t joystick_is_right(JoyStatus s);
+
+JoyStatus joystick_risen(uint8_t id);
+JoyStatus joystick_status(uint8_t id);
+
+#endif
