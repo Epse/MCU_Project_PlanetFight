0895576642bac28e5c25196bf5936a22d8d95ed9
Stef Pletinck <s@stefpletinck.be>
2019-04-15 12:13:44 +0200

    Added debug mode
    See #10

--- src/main.c
+++ src/main.c
@@ -11,6 +11,10 @@
 #include "dwenguinoBoard.h"
 #include "dwenguinoLCD.h"
 
+// Will be false when in debug mode
+uint8_t normal_run = 0xFF;
+uint16_t tick_count = 0;
+
 void setup()
 {
   // global enable interrupt
@@ -25,27 +29,63 @@ void setup()
 
   // Set up TC0 for ticking
   // in CTC mode, max value 255 (which gives 60Hz update rate)
+  // this is kinda fast, but I can't go slower.
   TCCR0A = _BV(WGM01);
   TCCR1A = _BV(CS02) | _BV(CS00);
   OCR0A = 0xFF;
   TIMSK0 = _BV(OCIE0A);
+
+  // Set the Center button to Input
+  DDRC &= ~_BV(7);
+  // Enable pullup
+  PORTC |= _BV(7);
 }
 
 void tick_interrupt() {
   //set_rotation_time(<sth>);
-  tick();
+  //tick();
+  tick_count++;
 }
 
 int main(void)
 {
   setup();
+  uint8_t center_pressed_for = 0;
   while (1)
   {
+    clearLCD();
     //set_rotation_time(<sth>);
     //render(<time_since_zero>)
+
+    // If the center button is pressed..
+    if ((~PINC) & _BV(7)) {
+      center_pressed_for++;
+    } else {
+      if (center_pressed_for >= 20) { // If it has been pressed long enough
+        normal_run = !normal_run; // Toggle between normal and debug
+      } else if (center_pressed_for && !normal_run) {
+      // If the button had been pressed, but is now released
+      // and we are not normally running
+        tick_interrupt(); // Manually tick
+      }
+      center_pressed_for = 0;
+    }
+
+    if (normal_run) {
+      printStringToLCD("NORMAL", 0, 0);
+    } else {
+      printStringToLCD("DEBUG", 0, 0);
+    }
+
+    printIntToLCD((int) tick_count, 1, 0);
+
+    // Remove this when render works
+    _delay_ms(50);
   }
 }
 
 ISR(TIMER0_COMPA_vect) {
-  tick_interrupt();
+  if (normal_run) {
+    tick_interrupt();
+  }
 }
