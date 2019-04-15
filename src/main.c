17da88e3f2ec73436cb67adb9b3d42a9821ab338
Stef Pletinck <s@stefpletinck.be>
2019-04-15 10:59:43 +0200

    Main templated out
    There are still some parts that have to be filled in,
    but `main.c` seems to be good.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <Position.h>
 #include "basics.h"
 #include "graphics.h"
+#include "engine.h"
 #include "leddriver.h"
 #include <Joystick.h>
 #include "dwenguinoBoard.h"
@@ -20,6 +21,19 @@ void setup()
   initLCD();
   clearLCD();
   backlightOn();
+  engine_setup();
+
+  // Set up TC0 for ticking
+  // in CTC mode, max value 255 (which gives 60Hz update rate)
+  TCCR0A = _BV(WGM01);
+  TCCR1A = _BV(CS02) | _BV(CS00);
+  OCR0A = 0xFF;
+  TIMSK0 = _BV(OCIE0A);
+}
+
+void tick_interrupt() {
+  //set_rotation_time(<sth>);
+  tick();
 }
 
 int main(void)
@@ -27,8 +41,11 @@ int main(void)
   setup();
   while (1)
   {
-    clearLCD();
-    joy_tick();
-    _delay_ms(50);
+    //set_rotation_time(<sth>);
+    //render(<time_since_zero>)
   }
+}
+
+ISR(TIMER0_COMPA_vect) {
+  tick_interrupt();
 }
