070463906304edf92f90f1f7edf548aa803efe25
Stef Pletinck <s@stefpletinck.be>
2019-05-03 15:50:20 +0200

    remove nonfunctioning code

--- src/graphics.c
+++ src/graphics.c
@@ -8,34 +8,7 @@ uint8_t close_enough(uint16_t realAngle, uint16_t angle, GraphicsSettings *set) 
   }
 }
 
-// Mja dees is nie zo generiek
-// ma afhankelijk van de schijf
-// Jammer dan
-// FIXME: Update for new disc
-uint8_t radius_lookup(uint8_t index) {
-  // Dit is voor als de schijf rechtsom draait
-  uint8_t lut[16] = {
-    15, 0, 8, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7
-  };
-
-  return lut[index];
-}
-
 void draw(Sprite *objects, uint8_t count, uint16_t time, GraphicsSettings *set) {
-  uint16_t angle = (time * A_LIMIT)/set->rotationTime;
-  uint8_t offset = A_LIMIT / (set->ledCount);
-  Led leds[set->ledCount];
-
-  for (uint8_t i = 0; i < set->ledCount - 2; i++) {
-    for (uint8_t j = 0; j < count; j++) {
-
-      uint8_t radius = radius_lookup(i);
-
-      if (close_enough(angle + offset*i, objects[j].pos.angle, set)
-        && close_enough(radius, objects[j].pos.radius, set)) {
-        leds[i] = objects[j].led;
-      }
-    }
-  }
+  // TODO: Fill this in
   led_draw(set->ledCount, leds);
 }
